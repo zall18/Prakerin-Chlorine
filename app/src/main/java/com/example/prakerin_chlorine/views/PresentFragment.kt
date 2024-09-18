b0c5d7c837eac1ae090cee6cadcf2912851a2e76
zall18 <121038973+zall18@users.noreply.github.com>
2024-09-19 06:07:55 +0700

    absence fitur

--- app/src/main/java/com/example/prakerin_chlorine/views/PresentFragment.kt
+++ app/src/main/java/com/example/prakerin_chlorine/views/PresentFragment.kt
@@ -1,33 +1,21 @@
 package com.example.prakerin_chlorine.views
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.PopupMenu
+import com.example.prakerin_chlorine.EditPofileActivity
+import com.example.prakerin_chlorine.HistoryAbsenceActivity
 import com.example.prakerin_chlorine.R
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [PresentFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class PresentFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
     }
 
     override fun onCreateView(
@@ -38,23 +26,33 @@ class PresentFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_present, container, false)
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment PresentFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            PresentFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        val option: ImageView = view.findViewById(R.id.option)
+
+        option.setOnClickListener {
+            popupMenu(option);
+        }
     }
+    private fun popupMenu(view: View) {
+        var popupMenu = PopupMenu(requireContext(), view)
+        popupMenu.menuInflater.inflate(R.menu.option_present, popupMenu.menu)
+
+        popupMenu.setOnMenuItemClickListener {
+                item ->
+            when(item.itemId){
+                R.id.history -> {
+                    startActivity(Intent(requireContext(), HistoryAbsenceActivity::class.java))
+                    true
+                }
+                else -> false
+            }
+        }
+
+        popupMenu.setOnDismissListener {
+            popupMenu.dismiss()
+        }
+
+        popupMenu.show()
+    }
+
 }
