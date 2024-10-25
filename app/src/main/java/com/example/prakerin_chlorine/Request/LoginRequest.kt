c8e4e788c98ed2ddf66a1d2f4869e633e942f4c9
akasshy <muhamadrizalf1112@gmail.com>
2024-10-25 16:59:44 +0700

    second changes

--- app/src/main/java/com/example/prakerin_chlorine/Request/LoginRequest.kt
+++
@@ -1,3 +0,0 @@
-package com.example.prakerin_chlorine.Request
-
-data class LoginRequest(var username: String, var password: String)
--- app/src/main/java/com/example/prakerin_chlorine/Response/TaskResponse.kt
+++
@@ -1,8 +0,0 @@
-package com.example.prakerin_chlorine.Response
-
-import com.example.prakerin_chlorine.Model.Task
-
-data class TaskResponse(
-    val message: String,
-    val task: MutableList<Task>
-)
